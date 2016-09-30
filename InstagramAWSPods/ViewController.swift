bff89be3de22828aa7dae897eb58b19c4dc94365
Wfuger <wfuger@gmail.com>
2016-09-30 14:33:02 -0500

    revert to swift 2

--- InstagramAWSPods/ViewController.swift
+++ InstagramAWSPods/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 import GoogleSignIn
+import AWSCore
+import AWSCognito
 
-class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
+
+class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate, AWSIdentityProviderManager {
+    
+    var googleIdToken = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +28,99 @@ class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if ( error == nil )
-        {
-            let idToken = user.authentication.idToken
-            let name = user.profile.name
-            let email = user.profile.email
-            
-            print(idToken, name, email)
-        }
-        else
-        {
+    
+    // SWIFT 3
+//    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+//        if ( error == nil )
+//        {
+//            googleIdToken = user.authentication.idToken
+//            signInToCognito(user: user)
+//        }
+//        else
+//        {
+//            print("\(error.localizedDescription)")
+//        }
+//    }
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!, withError error: NSError!) {
+        if error == nil {
+            googleIdToken = user.authentication.idToken
+            signInToCognito(user)
+        } else {
             print("\(error.localizedDescription)")
         }
     }
+    func logins() -> AWSTask {
+        let result = NSDictionary(dictionary: [AWSIdentityProviderGoogle: googleIdToken])
+        return AWSTask(result: result)
+    }
+    
+    func signInToCognito(user: GIDGoogleUser!) {
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType: .USWest2, identityPoolId: "us-west-2:6f1a621a-1357-4176-830a-936a633acba5", identityProviderManager: self)
+        
+        let config = AWSServiceConfiguration(region: .USWest2, credentialsProvider: credentialsProvider)
+        
+        AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = config
+        
+        credentialsProvider.getIdentityId().continueWithBlock { (task: AWSTask) -> AnyObject? in
+            if task.error != nil {
+                print(task.error)
+                return nil
+            }
+            let syncClient = AWSCognito.defaultCognito()
+            let dataset = syncClient.openOrCreateDataset("instagramDataSet")
+            
+            dataset.setString(user.profile.name, forKey: "name")
+            dataset.setString(user.profile.email, forKey: "email")
+            
+            let result = dataset.synchronize()
+            
+            result.continueWithBlock({ (task: AWSTask) -> AnyObject? in
+                if task.error != nil {
+                    print(task.error)
+                } else {
+                    print(task.result)
+                }
+                return nil
+            })
+            return nil
+        }
+    }
+    
+    
+    // SWIFT 3
+//    
+//    func signInToCognito(user: GIDGoogleUser!) {
+//        let credentialsProvider = AWSCognitoCredentialsProvider(regionType: .usWest2, identityPoolId: "us-west-2:6f1a621a-1357-4176-830a-936a633acba5", identityProviderManager: self)
+//        
+//        let config = AWSServiceConfiguration(region: .usWest2, credentialsProvider: credentialsProvider)
+//        
+//        AWSServiceManager.default().defaultServiceConfiguration = config
+//        
+//        credentialsProvider.getIdentityId().continue( { (task: AWSTask) -> Any? in
+//            if task.error != nil {
+//                print(task.error)
+//                return nil
+//            }
+//            let syncClient = AWSCognito.default()
+//            let dataset = syncClient?.openOrCreateDataset("instagramDataSet")
+//            
+//            dataset?.setString(user.profile.email, forKey: "email")
+//            dataset?.setString(user.profile.name, forKey: "name")
+//            
+//            let result = dataset?.synchronize()
+//            
+//            result?.continue({ (task: AWSTask) -> Any? in
+//                if task.error != nil {
+//                    print(task.error)
+//                } else {
+//                    print(task.result)
+//                }
+//                
+//                return nil
+//            })
+//            
+//            return nil
+//        })
 
 }
 
